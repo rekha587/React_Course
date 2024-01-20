9c42f8973b717b3cc68894a35d55e45c92e9cca4
RekhaV <vrrekha13@gmail.com>
2024-01-20 12:21:40 +0530

    First react Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
+const content = [
+  [
+    "Hello welcome", "have fun", "HIIIIIIIIIIIIII"
+  ],
+  [
+    "Hi Hello",
+    "React is fun",
+  ],
+  [
+    "Vanilla java script library",
+    "reat is a java script library"
+  ],
+  [
+    "React embraces declartive programming"
+  ]
+]
 
 function App() {
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,15 +29,41 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="tabs">
+          <menu>
+            <button className={activeContentIndex === 0 ? "active" : ""}
+              onClick={() => setActiveContentIndex(0)}
+            >
+              Why React?
+            </button>
+
+            <button className={activeContentIndex === 1 ? "active" : ""}
+              onClick={() => setActiveContentIndex(1)}
+            >
+              Core Features
+            </button>
+            <button className={activeContentIndex === 2 ? "active" : ""}
+              onClick={() => setActiveContentIndex(2)}
+            >
+              Related Resoruces
+            </button>
+            <button className={activeContentIndex === 3 ? "active" : ""}
+              onClick={() => setActiveContentIndex(3)}
+            >
+              React Facts
+            </button>
+          </menu>
+
+        </div>
+        <div id="tabContents">
+          <ul>
+            {content[activeContentIndex].map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
       </header>
+
     </div>
   );
 }
